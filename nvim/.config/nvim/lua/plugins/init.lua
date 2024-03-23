ee35cb055f846b1a6b56e26cd3efed80bb05e615
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2024-03-23 14:18:21 -0300

    feat: change nvim-surround keymaps

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -27,7 +27,21 @@ return {
   {
     "kylechui/nvim-surround",
     version = "*",
-    config = true,
+    opts = {
+      keymaps = {
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "ys",
+        normal_cur = "yss",
+        normal_line = "yS",
+        normal_cur_line = "ySS",
+        visual = "<leader>s",
+        visual_line = "g<leader>s",
+        delete = "ds",
+        change = "cs",
+        change_line = "cS",
+      },
+    },
   },
   {
     "numToStr/Comment.nvim",
