0d07619297265dd384dbb58533ee0c9dafbd1af4
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2024-05-28 20:56:27 -0300

    feat: update nvim config

--- nvim/.config/nvim/lua/plugins/mini_files.lua
+++ nvim/.config/nvim/lua/plugins/mini_files.lua
@@ -26,4 +26,21 @@ return {
       { noremap = true, silent = true },
     },
   },
+  init = function()
+    vim.api.nvim_create_autocmd("BufEnter", {
+      group = vim.api.nvim_create_augroup("MiniFilesStartDirectory", { clear = true }),
+      desc = "Start Mini Files with directory",
+      once = true,
+      callback = function()
+        if package.loaded["mini.files"] then
+          return
+        else
+          local stats = vim.uv.fs_stat(vim.fn.argv(0))
+          if stats and stats.type == "directory" then
+            require("mini.files").open(vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":p:h"), false)
+          end
+        end
+      end,
+    })
+  end,
 }
