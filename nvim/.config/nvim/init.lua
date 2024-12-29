156f1d6d77602bbafce31b4634bc00e86fd5b1ef
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2024-12-29 18:24:19 -0300

    feat: add neovide configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,16 @@
 require("basic")
 require("keybinds")
+
+if vim.g.neovide then
+  vim.o.guifont = "JetBrainsMono Nerd Font:h18"
+  local alpha = function()
+    return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
+  end
+  vim.g.neovide_transparency = 0.0
+  vim.g.transparency = 0.8
+  vim.g.neovide_background_color = "#1e1e2e" .. alpha()
+
+  vim.g.neovide_hide_mouse_when_typing = true
+
+  vim.g.neovide_cursor_vfx_mode = "sonicboom"
+end
