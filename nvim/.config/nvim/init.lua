cc68ffc40115d46dc2fb783d3ded0f1456cdeff7
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2025-04-26 23:47:27 -0300

    feat: update neovim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,7 +6,7 @@ if vim.g.neovide then
   local alpha = function()
     return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
   end
-  vim.g.neovide_transparency = 0.0
+  vim.g.neovide_opacity = 0.0
   vim.g.transparency = 0.8
   vim.g.neovide_background_color = "#1e1e2e" .. alpha()
 
