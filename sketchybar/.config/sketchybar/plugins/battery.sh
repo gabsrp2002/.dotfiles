446d8d49ee63df1bc4482515c1b4ae8e7d49f0b8
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2025-04-28 18:22:36 -0300

    feat: enhance sketchybar coloring

--- sketchybar/.config/sketchybar/plugins/battery.sh
+++ sketchybar/.config/sketchybar/plugins/battery.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+source "$CONFIG_DIR/colors.sh"
+
 PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
 CHARGING="$(pmset -g batt | grep 'AC Power')"
 
@@ -19,10 +21,19 @@ case "${PERCENTAGE}" in
   *) ICON=""
 esac
 
+case "${PERCENTAGE}" in
+  [6-8][0-9]|100) ICON_COLOR=$GREEN
+  ;;
+  [3-5][0-9]) ICON_COLOR=$YELLOW
+  ;;
+  *) ICON_COLOR=$RED
+esac
+
 if [[ "$CHARGING" != "" ]]; then
   ICON=""
+  ICON_COLOR=$YELLOW
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%" icon.color="$ICON_COLOR"
